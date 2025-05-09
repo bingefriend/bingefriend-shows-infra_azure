016cb650fd863fac4577289148b5acc73bdf33ad
Tom Boone <tbc@tomboone.com>
2025-05-09 15:38:28 -0400

    fix internal imports

--- function_app.py
+++ function_app.py
@@ -2,8 +2,8 @@
 
 # noinspection PyPackageRequirements
 import azure.functions as func
-from bingefriend.shows.infra_azure.blueprints.bp_ingest import bp as ingest_bp
-from bingefriend.shows.infra_azure.blueprints.bp_update import bp as update_bp
+from src.bingefriend.shows.infra_azure.blueprints.bp_ingest import bp as ingest_bp
+from src.bingefriend.shows.infra_azure.blueprints.bp_update import bp as update_bp
 
 app = func.FunctionApp()
 
